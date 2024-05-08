40e9e7a06daedf66aacd85c022c59fd5c57be0a8
ziaulhuq <ziaulhuqbe@gmail.com>
2024-05-08 09:57:03 +0800

    pagination

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Places, useState } from "./places";
 
 const Pagination = () => {
-  return <div>Pagination</div>;
+  const [tab, setTab] = useState([Places]);
+  return (
+    <div>
+      <button>Tab 1</button>
+      <button>Tab 2</button>
+      <button>Tab 3</button>
+      <button>Tab 4</button>
+
+      <div>
+        <ul></ul>
+      </div>
+    </div>
+  );
 };
 
 export default Pagination;
---
+++ src/Components/Pagination/places.js
@@ -0,0 +1,14 @@
+export const Places = [
+  [
+    "lorem Consequat cillum id consectetur aliquip officia id elit aliquip qui enim irure anim. Non anim culpa ex amet excepteur ea. Qui pariatur proident ex ea commodo eiusmod aliquip incididunt non ullamco elit ipsum sint officia.",
+  ],
+  [
+    "lorem Consequat cillum id consectetur aliquip officia id elit aliquip qui enim irure anim. Non anim culpa ex amet excepteur ea. Qui pariatur proident ex ea commodo eiusmod aliquip incididunt non ullamco elit ipsum sint officia.",
+  ],
+  [
+    "lorem Consequat cillum id consectetur aliquip officia id elit aliquip qui enim irure anim. Non anim culpa ex amet excepteur ea. Qui pariatur proident ex ea commodo eiusmod aliquip incididunt non ullamco elit ipsum sint officia.",
+  ],
+  [
+    "lorem Consequat cillum id consectetur aliquip officia id elit aliquip qui enim irure anim. Non anim culpa ex amet excepteur ea. Qui pariatur proident ex ea commodo eiusmod aliquip incididunt non ullamco elit ipsum sint officia.",
+  ],
+];
